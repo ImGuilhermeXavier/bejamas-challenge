7cf8909df9a3f811ca53eb919030a3387b686177
Guilherme Caçador <gxavier@esapiens.com.br>
2021-11-23 14:15:07 -0300

    Fazer o retorno da api

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import api from 'api'
+import React, { useState } from 'react'
 
 function Home() {
+    const [products, setProducts] = useState()
+    React.useEffect(() => {
+        async function getProducts() {
+            const { data: res } = await api.get('products')
+            setProducts(res.data.map((product: any) => product.data))
+        }
+        getProducts()
+    }, [])
+
+    console.log(products)
+
     return <div></div>
 }
 
