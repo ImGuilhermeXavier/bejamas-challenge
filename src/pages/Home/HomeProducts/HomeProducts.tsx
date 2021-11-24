3f93946434c0e0c0f12a9b8feb4680307117e222
Guilherme Caçador <gxavier@esapiens.com.br>
2021-11-24 17:07:57 -0300

    Start to list products

--- src/pages/Home/HomeProducts/HomeProducts.tsx
+++ src/pages/Home/HomeProducts/HomeProducts.tsx
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './HomeProducts.module.scss'
 
 function HomeProducts() {
+    const [premium, setPremium] = useState<boolean>(false)
+
+    useEffect(() => {
+        console.log('change Best seller')
+    }, [premium])
+
     return (
         <section className={styles.homeProducts}>
             <header className={styles.header}>
-                <h2>Photography / Premium Photos</h2>
-                <h3>Sort By Price</h3>
+                <h2 className={styles.options}>
+                    <button
+                        className={`${styles.optionButton} ${
+                            !premium ? styles.optionSelected : ''
+                        }`}
+                        onClick={() => setPremium(false)}
+                    >
+                        Photography
+                    </button>{' '}
+                    /
+                    <button
+                        className={`${styles.optionButton} ${
+                            premium ? styles.optionSelected : ''
+                        }`}
+                        onClick={() => setPremium(true)}
+                    >
+                        Premium Photos
+                    </button>
+                </h2>
+                <h3 className={styles.options}>
+                    Sort By{' '}
+                    <button
+                        className={`${styles.optionButton} ${styles.optionButtonSm}`}
+                    >
+                        Price
+                    </button>
+                </h3>
             </header>
+
+            <section className="category"></section>
+            <section className="products"></section>
         </section>
     )
 }
